9b46a7e3009c1aff6f834d2457f1428f08479922
AugAid <AugAid24@gmail.com>
2024-07-27 17:47:51 +0530

    Second Commit

--- script.js
+++ script.js
@@ -1,1 +1,10 @@
-"" 
+// Function to check if "username" is in localStorage and redirect if not
+function checkLogin() {
+    const username = localStorage.getItem('username');
+    if (!username) {
+        window.location.href = '/login';
+    }
+}
+
+// Call the function on page load
+checkLogin();
